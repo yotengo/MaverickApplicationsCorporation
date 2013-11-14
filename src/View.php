693ebd5267757a175f176c95d1fcb9f2dbb0c4f5
Kevin Rock <krock15@huskers.unl.edu>
2013-11-14 15:15:01 -0600

    modified the View slightly

--- src/View.php
+++ src/View.php
@@ -4,5 +4,12 @@
  * View.php
  */
 
-
+		/* 
+		*	A function that given a list/array of posts, 
+		*	will display them chronicalogically with newest at the top
+		*/
+		function display Posts($PostList){
+		
+		
+		}
 ?>
