81d430b95b69d9d59c01005e47b5187592af4eda
Kevin Rock <krock15@huskers.unl.edu>
2013-11-16 11:05:13 -0600

    started working on Header

--- src/View.php
+++ src/View.php
@@ -6,10 +6,45 @@
 
 		/* 
 		*	A function that given a list/array of posts, 
-		*	will display them chronicalogically with newest at the top
+		*	will display them chronologically with newest at the top
 		*/
-		function display Posts($PostList){
+		function displayPosts($PostList){
 		
 		
 		}
+		
+		
+		/*
+		* This function will display the title bar at the top of each page.
+		* Note: The 50 pixels for the header is pretty arbitrary right now and
+		* can be sorted out later.
+		*/
+		function displayHeader($page){
+		echo PHP_EOL."<html>".PHP_EOL;
+		echo PHP_EOL."<frameset rows=\"50,100%\">".PHP_EOL;
+		echo "<frame src=\"header.html\">".PHP_EOL;
+		echo "<frame src=\"".$page."\">".PHP_EOL;
+		echo "</frameset>".PHP_EOL;
+		echo "</html>".PHP_EOL;	
+		}
+		
+		function testFunc(){
+		
+			echo "<br/>will this work?". "<br/>".PHP_EOL;
+			echo "This is test function.".PHP_EOL;
+			
+		}
+		
+		
+		$dispage="login.php";
+		
+		displayHeader($dispage);
+		
+		echo "This works.";
+		
+		echo "<b> as does this </b>";
+		
+		
+		testFunc();
+		
 ?>
